2e0bddf2fac2ef7bd34a7b2eae978401d35e871e
Pasha-nineseven <pavel@nineseven.ru>
2018-12-05 17:47:32 +0300

    about-full

--- app/js/interface.js
+++ app/js/interface.js
@@ -1,31 +1,35 @@
 $(document).ready(function() {
 	flexibility(document.documentElement);
-	// $("body").on("click", ".test", function(e){
-	// 	e.preventDefault();
-	// })
+    //TOP-MENU
+	$('body').on('click','.menu-btn', function(e){
+        e.preventDefault();
+        $(this).toggleClass('active');
+        $('.menu').fadeToggle();
+        $('body').toggleClass('hidden');
+    });
+
+    //TOP-SEARCH
+    $("body").on("click", ".header-search__button", function(e){
+        e.preventDefault();
+        $('.page-header-search').toggleClass('open');
+        $('.page-search').toggleClass('open');
+        $('body').toggleClass('hidden-scroll');
+    });
 
 
-    //PAGE-SEARCH
-    // $('.page-header__input').keyup(function(){
-    //   var $this = $(this),
-    //   val = $this.val();
-    //   if(val.length >= 3){
-    //     $('.page-header__result').fadeIn(20);
-    //     $('body').addClass('hidden');
-    //     $('.page-header__search').addClass('active');
-    //   }else {
-    //     $('.page-header__result').fadeOut(20);
-    //     $('.page-header__search').removeClass('active');
-    //   }
-    // });
+    $("body").on("click", ".video__play", function(e){
+        $(this).hide();
 
+        $(this).prev('.video__poster').hide();
 
-    // $("body").on("click", ".page-header__bg", function(){
-    //     $('.page-header__result').fadeOut(20);
-    //     $('body').removeClass('hidden');
-    //     $('.page-header__search__input').val("");
-    //     $('.page-header__search').removeClass('active');
-    // });
+        //$('#video')[0].src += "&autoplay=1";
+
+        var videoURL = $('#video').prop('src');
+        videoURL += "&autoplay=1";
+        $('#video').prop('src',videoURL);
+
+        e.preventDefault();
+    });
 
 
 	//LANGUAGE
@@ -46,6 +50,21 @@ $(document).ready(function() {
 
 
 
+    //FOOTER-LANGUAGE
+    $("body").on("click", ".page-footer-lang__view", function(e){
+        if (!$(e.target).hasClass('page-header-lang__item')) {
+          e.preventDefault();
+        }
+        $('.page-footer-lang__list').fadeToggle(100);
+    });
+    $(document).click(function (e){
+        var div = $(".page-footer-lang");
+        if (!div.is(e.target)
+            && div.has(e.target).length === 0) {
+          $(".page-footer-lang__list").fadeOut(100);
+        }
+    });
+
     
 });
 
@@ -58,38 +77,38 @@ $(window).resize(function () {
 
 
 //HEADER-SCROLL
-//     var didScroll;
-//     var lastScrollTop = 0;
-//     var delta = 5;
-//     var navbarHeight = 87;
+    var didScroll;
+    var lastScrollTop = 0;
+    var delta = 5;
+    var navbarHeight = 87;
 
-// $(window).scroll(function(event){
-//     didScroll = true;
-// });
+$(window).scroll(function(event){
+    didScroll = true;
+});
 
-// setInterval(function() {
-//     if (didScroll) {
-//         hasScrolled();
-//         didScroll = false;
-//     }
-// }, 250);
+setInterval(function() {
+    if (didScroll) {
+        hasScrolled();
+        didScroll = false;
+    }
+}, 250);
 
-// functions
-// function hasScrolled() {
-//     var st = $(this).scrollTop();
+//functions
+function hasScrolled() {
+    var st = $(this).scrollTop();
 
-//     if(Math.abs(lastScrollTop - st) <= delta)
-//         return;
+    if(Math.abs(lastScrollTop - st) <= delta)
+        return;
 
-//     if (st > lastScrollTop && st > navbarHeight){
-//         $('header:not(.open)').removeClass('nav-down').addClass('nav-up');
-//     } else {
-//         if(st + $(window).height() < $(document).height()) {
-//             $('header').removeClass('nav-up').addClass('nav-down');
-//         }
-//     }
-//     lastScrollTop = st;
-// }
+    if (st > lastScrollTop && st > navbarHeight){
+        $('header:not(.open)').removeClass('nav-down').addClass('nav-up');
+    } else {
+        if(st + $(window).height() < $(document).height()) {
+            $('header').removeClass('nav-up').addClass('nav-down');
+        }
+    }
+    lastScrollTop = st;
+}
 
 
 
