6428d4b9dd0f0e6b465af56a7549456e01a9b03a
Egor Shevchik <want2know87@gmail.com>
2019-03-12 17:28:09 +0300

    override.css

--- gulpfile.js
+++ gulpfile.js
@@ -95,7 +95,8 @@ gulp.task('build', ['clean', 'img', 'sass', 'css-libs', 'scripts'], function() {
 
     var buildCss = gulp.src([
         'app/css/style.css',
-        'app/css/libs.min.css'
+        'app/css/libs.min.css',
+        'app/css/override.css'
         ])
     .pipe(gulp.dest('dist/css'))
 
