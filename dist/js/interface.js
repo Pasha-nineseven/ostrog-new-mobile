304240240c493e6895df20c8d3dd8ff6149d79d7
Pavel-nineseven <pavel@nineseven.ru>
2019-04-04 14:04:33 +0300

    +

--- dist/js/interface.js
+++ dist/js/interface.js
@@ -22,6 +22,32 @@ $(document).ready(function() {
         $('#searchMe').focus();
     });
 
+    if ($('.contents').length>0) {
+        var lastY = 0; // Needed in order to determine direction of scroll.
+        $(".contents__unit").on('touchstart', function(event) {
+            lastY = event.touches[0].clientY;
+        });
+
+        $('.contents__unit').on('touchmove', function(event) {
+            var top = event.touches[0].clientY;
+
+            // Determine scroll position and direction.
+            var scrollTop = $(event.currentTarget).scrollTop();
+            var direction = (lastY - top) < 0 ? "up" : "down";
+
+            // FIX IT!
+            if (scrollTop == 0 && direction == "up") {
+              // Prevent scrolling up when already at top as this introduces a freeze.
+              event.preventDefault();
+            } else if (scrollTop >= (event.currentTarget.scrollHeight - event.currentTarget.outerHeight()) && direction == "down") {
+              // Prevent scrolling down when already at bottom as this also introduces a freeze.
+              event.preventDefault();
+            }
+
+            lastY = top;
+        });
+    }
+
     $("body").on("click", ".header-search__button", function(e){
         e.preventDefault();
         $('.page-header-search').toggleClass('open');
